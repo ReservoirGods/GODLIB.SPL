157ab322239ab666ff9de4be11283a1f08919818
pink-rg <leon_oreilly@yahoo.com>
2018-09-03 17:04:46 -0400

    use a mapped drive for more efficient fastbuild cacheing

--- buildall.bat
+++ buildall.bat
@@ -1,6 +1,22 @@
-rem @echo off
+:: Resrvoir Gods GODLIB.SPL builder
+::
+:: Build all samples using fastbuild
+::
+:: We create a common base drive (X:) to allow fastbuild cache to be shared between projects
+
+@echo off
 
 setlocal
+
+pushd ..
+
+set mypath=%cd%
+
+subst X: %mypath%
+
+pushd X:
+
+cd GODLIB.SPL
 
 CALL :buildIt BOX %*
 CALL :buildIt CLI %*
@@ -12,6 +28,12 @@ CALL :buildIt PICSHOW %*
 CALL :buildIt SCREEN %*
 CALL :buildIt SCRNSWAP %*
 CALL :buildIt SPRITE %*
+
+popd
+
+subst X: /D
+
+popd
 
 endlocal
 
