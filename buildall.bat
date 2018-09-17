eab459487f4874e59e7979d9938427507155adae
pink-rg <leon_oreilly@yahoo.com>
2018-09-16 20:34:37 -0400

    fix problematic output paths in PRJ files now dynamic buildall script

--- buildall.bat
+++ buildall.bat
@@ -5,48 +5,28 @@
 :: We create a common base drive (X:) to allow fastbuild cache to be shared between projects
 
 @echo off
-
 setlocal
 
-pushd ..
-
-set mypath=%cd%
-
+::pushd ..
+::set mypath=%cd%
 ::subst X: %mypath%
-
 ::pushd X:
+::cd GODLIB.SPL
 
-cd GODLIB.SPL
-
-CALL :buildIt AUDIO %*
-CALL :buildIt BOX %*
-CALL :buildIt CLI %*
-CALL :buildIt COOKIJAR %*
-CALL :buildIt JAGPAD %*
-CALL :buildIt JOY %*
-CALL :buildIt MOUSE %*
-CALL :buildIt MUSIC %*
-CALL :buildIt PICSHOW %*
-CALL :buildIt SCREEN %*
-CALL :buildIt SCRNSWAP %*
-CALL :buildIt SPRITE %*
-
+@echo off
+pushd
+for /D %%i in (*) do (
+	if exist "%%i\FAST" (
+		pushd "%%i\FAST"
+		call build.bat %*
+		popd
+	)
+)
 popd
 
+
+::popd
 ::subst X: /D
-
-popd
+::popd
 
 endlocal
-
-exit /B 0
-
-:buildIt
-
-pushd "%~1\FAST" 
-shift
-for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
-call build.bat %ALL_BUT_FIRST%
-popd
-
-exit /B 0
